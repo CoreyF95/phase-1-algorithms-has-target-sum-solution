df30db6760ae824f975e4fef6e443ae506712e7f
Corey Fisher <coreyfisher2021@gmail.com>
2021-12-01 15:19:46 -0600

    commit tree 00cbcb901c89806d24c2e0140343abaea49cb6c0

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set()
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers[complement]) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time complexity: O(n)
+  Space complexity: O(n)
 */
 
 /* 
   Add your pseudocode here
+
+  Create function
+    Create a new Set
+    Iterate through the array
+      Create varible equal to target minus first iteration
+      If new Set contains the new variable, return true
+        If Set does not contain the new variable, return false
 */
 
 /*
-  Add written explanation of your solution here
+  1.) Create a function that takes two arguments, an array and a target.
+  2.) Create a Set that contains the numbers that have been iterated through.
+  3.) Create a variable that has a value of the target minus the iteration.
+  5.) If the Set contains the new variable, return true.
+  6.) If not true, return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,12 +38,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 9, 15, 43, 67], 14))
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 52, 96, 38, 25, 76], 100))
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
